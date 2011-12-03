875c79af0836bf86b94c1c56ac6d450c2104c51c
Max Löffler <max.loeffler@gmail.com>
2011-12-03 14:11:52 +0000

    latex template update
    git-svn-id: http://svn.code.sf.net/p/cvtx/code-0/trunk@23 c54afaf7-fb21-4bc2-8220-74f7e507a662

--- themes/cvtx/latex/single-cvtx_antrag.php
+++ themes/cvtx/latex/single-cvtx_antrag.php
@@ -6,28 +6,41 @@
 \usepackage{fixltx2e}
 %\usepackage[onehalfspacing]{setspace}
 \usepackage{lineno}
+\usepackage{tabularx}
+%\usepackage{calc}
 
 \sloppy
 \pagestyle{myheadings}
-\markright{<?php cvtx_top(); ?>}
+\markright{<?php cvtx_kuerzel(); ?> <?php cvtx_titel(); ?>}
 
 \begin{document}
 
+\thispagestyle{empty}
+
 \begin{flushright}
- <?php cvtx_name(); ?>\\
+ \textbf{\large <?php cvtx_name(); ?>}\\
  <?php cvtx_beschreibung(); ?>
 \end{flushright}
 
-\textbf{\Huge <?php cvtx_kuerzel(); ?>}
+%\newcommand*\adjust{\setlength\hsize{\textwidth-2\tabcolsep}}
+\begin{tabularx}{\textwidth}{|lX|}
+    \hline
+                                                &                                                                           \\
+%    \multicolumn{2}{|>{\adjust}X|}{\textbf{\LARGE <?php cvtx_kuerzel(); ?>} \textbf{\large <?php cvtx_top_titel(); ?>}}     \\
+    \textbf{\LARGE <?php cvtx_kuerzel(); ?>}    &   \textbf{\large <?php cvtx_top_titel(); ?>}                              \\
+                                                &                                                                           \\
+    AntragstellerInnen:                         &   <?php cvtx_antragsteller(); ?>                                          \\
+                                                &                                                                           \\
+    Gegenstand:                                 &   <?php cvtx_top_titel(); ?> (<?php cvtx_top_kuerzel(); ?>)               \\
+                                                &                                                                           \\
+    \hline
+\end{tabularx}
 
-\textbf{\LARGE <?php cvtx_titel(); ?>}
-
-\textbf{Gegenstand:} <?php cvtx_top_titel(); ?> (<?php cvtx_top_kuerzel(); ?>)\\
-\textbf{Antragsteller:} <?php cvtx_antragsteller(); ?>\\[0em]
+\textbf{\Large <?php cvtx_titel(); ?>}
 
 \begin{linenumbers}
-\modulolinenumbers[5]
-<?php cvtx_antragstext(); ?>\\[0em]
+%\modulolinenumbers[5]
+<?php cvtx_antragstext(); ?>
 \end{linenumbers}
 
 \textbf{Begründung:}\\
