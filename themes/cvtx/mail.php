7afacd8897497269a0b89a8cb77acb28c33171b7
Alex Fecke <alexander.fecke@googlemail.com>
2012-01-08 21:34:22 +0000

    small changes on the mail template
    git-svn-id: http://svn.code.sf.net/p/cvtx/code-0/trunk@108 c54afaf7-fb21-4bc2-8220-74f7e507a662

--- themes/cvtx/mail.php
+++ themes/cvtx/mail.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<table style="width:94%;border-radius:3px;margin-left:3%;margin-top:0.7em">
-			<tr><td id="header" style="height:150px; border-radius:10px;">
+			<tr><td id="header" style="height:150px; border-radius:5px;" class="mail">
 				<table id="verlauf" style="height:150px; border-radius:10px; background-position: center">
 					<tr>
 						<td id="headerimg" style="margin-left:40px; margin-top:30px">
@@ -17,11 +17,13 @@
 						<td class="description" style="padding-left:40px"><?php bloginfo('description'); ?></td>
      				</tr>
      			</table>
-				<div id="b90" style="position:absolute; right:8%; top:-10px"></div>
+				<div class="mail" style="position:absolute;right:8%;top:100px;background: url(<?php echo get_template_directory_uri(); ?>/images/b90_small.png); width: 80px; height: 42px;"></div>
 				</div>
 			</td></tr>
 		</table>
-		<table style="background:white;width:94%;border-radius:3px;margin-left:3%;margin-top:1em">
+		<table style="background:white;width:94%;border-radius:5px;margin-left:3%;margin-top:1em;box-shadow: 0px 0px 15px rgba(0,0,0,0.3);
+	-webkit-box-shadow: 0px 0px 15px rgba(0,0,0,0.3);
+	-moz-box-shadow: 0px 0px 15px rgba(0,0,0,0.3)" class="mail-content">
 			<tr><td style="padding:15px"><?php print $content; ?></td></tr>
 		</table>
 	</body>
