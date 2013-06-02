1b65e624981f575c3efbb4a95c2f63900007266e
mloeffler <max.loeffler@gmail.com>
2013-06-02 13:06:16 +0200

    Minor correction, run purifier for applications as well

--- plugins/cvtx/cvtx.php
+++ plugins/cvtx/cvtx.php
@@ -943,7 +943,7 @@ function cvtx_the_content($content) {
     global $post;
     
     // Sanitize content using HTMLPurifier-plugin
-    if (($post->post_type == 'cvtx_antrag' || $post->post_type == 'cvtx_aeantrag')
+    if (is_object($post) && ($post->post_type == 'cvtx_antrag' || $post->post_type == 'cvtx_aeantrag' || $post->post_type == 'cvtx_application')
      && is_plugin_active('html-purified/html-purified.php')) {
         global $cvtx_purifier, $cvtx_purifier_config;
         // Purify resolution text and meta fields
